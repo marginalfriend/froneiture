61c2a9ff54cef5913a78d9a95ba4d0ecabeceabb
marginalfriend <abdullahpsg@gmail.com>
2025-01-22 23:41:39 +0700

    gc

--- app/(admin)/admin/inquiry/page.tsx
+++ app/(admin)/admin/inquiry/page.tsx
@@ -4,7 +4,7 @@ import InquiriesTable from "./components/inquiries-table";
 const Page = () => {
   return (
     <div className="flex flex-col">
-      <h1 className="text-3xl font-semibold">Inquiry</h1>
+      <h1 className="text-3xl font-semibold">Order</h1>
       <InquiriesTable />
     </div>
   );
